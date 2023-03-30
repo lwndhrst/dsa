348c355a7e22a95728610b9412c66ef5a00c1e54
Leon Windhorst <leon@windhorst.dev>
2023-03-30 18:51:43 +0200

    add: scuffed min_heap implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,1 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    
-    #[test]
-    fn test_add() {
-        assert_eq!(4, add(2, 2));
-    }
-}
+pub mod min_heap;
