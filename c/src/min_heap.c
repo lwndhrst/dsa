4048f3ba08476c3cc65e45804752a23feea0b26e
Leon Windhorst <leon@windhorst.dev>
2023-04-30 09:06:05 +0200

    refactor: bunch of renaming

--- c/src/min_heap.c
+++ c/src/min_heap.c
@@ -2,37 +2,37 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define SIZE 1024
+#define MIN_HEAP_SIZE 1024
 
 typedef struct MinHeap {
     size_t len;
-    int data[SIZE];
+    int data[MIN_HEAP_SIZE];
 } MinHeap;
 
-size_t parent(size_t idx) {
+size_t min_heap_parent_idx(size_t idx) {
     return (idx - 1) / 2;
 }
 
-size_t left_child(size_t idx) {
+size_t min_heap_left_child_idx(size_t idx) {
     return 2 * idx + 1;
 }
 
-size_t right_child(size_t idx) {
+size_t min_heap_right_child_idx(size_t idx) {
     return 2 * idx + 2;
 }
 
-int heapify_up(MinHeap *heap, size_t idx) {
+int min_heap_heapify_up(MinHeap *heap, size_t idx) {
     size_t current_idx = idx;
     int current_val = heap->data[current_idx];
 
     while (current_idx > 0) {
-        size_t parent_idx = parent(current_idx);
+        size_t parent_idx = min_heap_parent_idx(current_idx);
         int parent_val = heap->data[parent_idx];
 
         if (current_val >= parent_val) 
             return 0;
 
-        heap->data[parent_idx] = current_val;
+        heap->data[parent_idx]  = current_val;
         heap->data[current_idx] = parent_val;
         current_idx = parent_idx;
     }
@@ -40,21 +40,21 @@ int heapify_up(MinHeap *heap, size_t idx) {
     return 0;
 }
 
-int heapify_down(MinHeap *heap, size_t idx) {
+int min_heap_heapify_down(MinHeap *heap, size_t idx) {
     size_t current_idx = idx;
     int current_val = heap->data[current_idx];
 
     while (current_idx < heap->len - 1) {
-        size_t left_idx = left_child(current_idx);
+        size_t left_idx = min_heap_left_child_idx(current_idx);
         if (left_idx >= heap->len)
             return 0;
 
-        size_t right_idx = right_child(current_idx);
+        size_t right_idx = min_heap_right_child_idx(current_idx);
         size_t min_idx;
         if (right_idx >= heap->len) {
             min_idx = left_idx;
         } else {
-            int left_val = heap->data[left_idx];
+            int left_val  = heap->data[left_idx];
             int right_val = heap->data[right_idx];
             min_idx = left_val < right_val ? left_idx : right_idx;
         }
@@ -63,7 +63,7 @@ int heapify_down(MinHeap *heap, size_t idx) {
         if (min_val >= current_val)
             return 0;
 
-        heap->data[min_idx] = current_val;
+        heap->data[min_idx]     = current_val;
         heap->data[current_idx] = min_val;
         current_idx = min_idx;
     }
@@ -71,15 +71,15 @@ int heapify_down(MinHeap *heap, size_t idx) {
     return 0;
 }
 
-int push(MinHeap *heap, int value) {
+int min_heap_push(MinHeap *heap, int value) {
     heap->data[heap->len] = value;
-    heapify_up(heap, heap->len);
+    min_heap_heapify_up(heap, heap->len);
     heap->len += 1;
 
     return 0;
 }
 
-int pop(MinHeap *heap) {
+int min_heap_pop(MinHeap *heap) {
     if (heap->len == 0)
         return -1;
 
@@ -92,15 +92,15 @@ int pop(MinHeap *heap) {
 
     heap->len -= 1;
     heap->data[0] = heap->data[heap->len];
-    heapify_down(heap, 0);
+    min_heap_heapify_down(heap, 0);
 
     return val;
 }
 
-int test_weak_order(MinHeap *heap, size_t max_idx) {
+int _test_weak_order(MinHeap *heap, size_t max_idx) {
     for (int i = 0; i < max_idx; i += 1) {
-        size_t left_idx = left_child(i);
-        size_t right_idx = right_child(i);
+        size_t left_idx  = min_heap_left_child_idx(i);
+        size_t right_idx = min_heap_right_child_idx(i);
 
         if (left_idx >= heap->len)
             continue;
@@ -128,17 +128,17 @@ int main(void) {
     MinHeap heap;
     heap.len = 0;
 
-    for (int i = 0; i < SIZE; i += 1) {
-        push(&heap, rand());
-        test_weak_order(&heap, i);
+    for (int i = 0; i < MIN_HEAP_SIZE; i += 1) {
+        min_heap_push(&heap, rand());
+        _test_weak_order(&heap, i);
     }
 
-    if (heap.len != SIZE)
-        printf("Expected: heap.len == %zu but got %d instead\n", heap.len, SIZE);
+    if (heap.len != MIN_HEAP_SIZE)
+        printf("Expected: heap.len == %zu but got %d instead\n", heap.len, MIN_HEAP_SIZE);
 
-    for (int i = 0; i < SIZE; i += 1) {
-        int val = pop(&heap);
-        test_weak_order(&heap, SIZE - i);
+    for (int i = 0; i < MIN_HEAP_SIZE; i += 1) {
+        int val = min_heap_pop(&heap);
+        _test_weak_order(&heap, MIN_HEAP_SIZE - i);
     }
 
     return 0;
